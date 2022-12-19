f965797ccc12bd32d12e28ab42133230d3fbe3b0
Henrik Reponen <henrik@rundit.com>
2022-12-19 16:25:28 +0200

    chore: add parameterized tests

--- test/xirr.spec.ts
+++ test/xirr.spec.ts
@@ -1,36 +1,40 @@
-var runditXirr = require('../index')
+import { Payment, XirrOptions } from '../src';
+import { testCases } from './test_cases';
+import { compute } from '../src/index';
 
 describe('xirr', () => {
-  const xirr = (payments: Payment[], opts?: any) => runditXirr.compute(payments, opts)
+  const xirr = (payments: Payment[], opts?: XirrOptions) =>
+    compute(payments, opts);
 
   test('calculate explosive returns', () => {
-    expect(runditXirr.compute([
-      { date: '2005-06-01', amount: -162000 },
-      { date: '2005-07-31', amount: 505000 },
-      { date: '2009-06-01', amount: -116000 },
-      { date: '2011-06-01', amount: -116000 },
-      { date: '2013-07-31', amount: 1825000 },
-      { date: '2022-01-15', amount: 8000000 },
-      { date: '2022-12-16', amount: 9000000 },
-    ])).toEqual(1007.8026438395354)
-  })
-
+    expect(
+      compute([
+        { date: '2005-06-01', amount: -162000 },
+        { date: '2005-07-31', amount: 505000 },
+        { date: '2009-06-01', amount: -116000 },
+        { date: '2011-06-01', amount: -116000 },
+        { date: '2013-07-31', amount: 1825000 },
+        { date: '2022-01-15', amount: 8000000 },
+        { date: '2022-12-16', amount: 9000000 },
+      ]),
+    ).toEqual(1007.8026438395354);
+  });
 
   it('computes the xirr on 1 year growth of 0%', () => {
-    const transactions = []
-    transactions.push({ amount: -1000, date: '2010-01-01' })
-    transactions.push({ amount: 1000, date: '2011-01-01' })
-    const result = xirr(transactions)
-    expect(result).toBeCloseTo(0)
-  })
+    const transactions = [];
+    transactions.push({ amount: -1000, date: '2010-01-01' });
+    transactions.push({ amount: 1000, date: '2011-01-01' });
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(0);
+  });
 
   it('computes the xirr on 1 year growth of 10%', () => {
-    const transactions = []
-    transactions.push({ amount: -1000, date: '2010-01-01' })
-    transactions.push({ amount: 1100, date: '2011-01-01' })
-    const result = xirr(transactions)
-    expect(result.toPrecision(6)).toEqual('0.100000')
-  })
+    const transactions = [];
+    transactions.push({ amount: -1000, date: '2010-01-01' });
+    transactions.push({ amount: 1100, date: '2011-01-01' });
+    const result = xirr(transactions);
+    expect(result.toPrecision(6)).toEqual('0.100000');
+  });
 
   it('computes the xirr on a particular data set the same as a popular spreadsheet', () => {
     const transactions = [
@@ -38,68 +42,66 @@ describe('xirr', () => {
       { amount: -1000, date: '2010-04-01' },
       { amount: -1000, date: '2010-07-01' },
       { amount: -1000, date: '2010-10-01' },
-      { amount: 4300, date: '2011-01-01' }
-    ]
-    const result = xirr(transactions)
-    expect(result.toPrecision(6)).toEqual('0.121268')
-  })
+      { amount: 4300, date: '2011-01-01' },
+    ];
+    const result = xirr(transactions);
+    expect(result.toPrecision(6)).toEqual('0.121268');
+  });
 
   it('gets the same answer even if the transactions are out of order', function () {
-    const transactions = []
-    transactions.push({ amount: -1000, date: '2010-10-01' })
-    transactions.push({ amount: 4300, date: '2011-01-01' })
-    transactions.push({ amount: -1000, date: '2010-07-01' })
-    transactions.push({ amount: -1000, date: '2010-01-01' })
-    transactions.push({ amount: -1000, date: '2010-04-01' })
+    const transactions = [];
+    transactions.push({ amount: -1000, date: '2010-10-01' });
+    transactions.push({ amount: 4300, date: '2011-01-01' });
+    transactions.push({ amount: -1000, date: '2010-07-01' });
+    transactions.push({ amount: -1000, date: '2010-01-01' });
+    transactions.push({ amount: -1000, date: '2010-04-01' });
 
-    const result = xirr(transactions)
-    expect(result.toPrecision(6)).toEqual('0.121268')
-  })
+    const result = xirr(transactions);
+    expect(result.toPrecision(6)).toEqual('0.121268');
+  });
 
   it('computes the negative xirr on 1 year decline of 10%', () => {
-    const transactions = []
-    transactions.push({ amount: -1000, date: '2010-01-01' })
-    transactions.push({ amount: 900, date: '2011-01-01' })
-    const result = xirr(transactions)
-    expect(result.toPrecision(6)).toEqual('-0.100000')
-  })
-
+    const transactions = [];
+    transactions.push({ amount: -1000, date: '2010-01-01' });
+    transactions.push({ amount: 900, date: '2011-01-01' });
+    const result = xirr(transactions);
+    expect(result.toPrecision(6)).toEqual('-0.100000');
+  });
 
   it('computes rates of return greater than 100%', () => {
     const transactions = [
       { amount: -1000, date: '2010-01-01' },
-      { amount: 3000, date: '2011-01-01' }
-    ]
-    const result = xirr(transactions)
-    expect(result).toBeCloseTo(2)
-  })
+      { amount: 3000, date: '2011-01-01' },
+    ];
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(2);
+  });
 
   it('computes a rate of return of -100% on a total loss', () => {
-    const transactions = []
-    transactions.push({ amount: -1000, date: '2010-01-01' })
-    transactions.push({ amount: 0, date: '2011-01-01' })
-    const result = xirr(transactions)
-    expect(result).toBeNaN()
-  })
+    const transactions = [];
+    transactions.push({ amount: -1000, date: '2010-01-01' });
+    transactions.push({ amount: 0, date: '2011-01-01' });
+    const result = xirr(transactions);
+    expect(result).toBeNaN();
+  });
 
   it('computes a rate of return of -100% on a total loss', () => {
     const transactions = [
       { amount: -1000, date: '2010-01-01' },
-      { amount: 0, date: '2012-01-01' }
-    ]
-    const result = xirr(transactions)
-    expect(result).toBeNaN()
-  })
+      { amount: 0, date: '2012-01-01' },
+    ];
+    const result = xirr(transactions);
+    expect(result).toBeNaN();
+  });
 
   it('computes a rate of return of -100% on a total loss', () => {
     const transactions = [
       { amount: -1000, date: '2010-01-01' },
       { amount: 0, date: '2010-08-01' },
-    ]
-    const result = xirr(transactions)
-    expect(result).toBeNaN()
-  })
-
+    ];
+    const result = xirr(transactions);
+    expect(result).toBeNaN();
+  });
 
   it('does not error out on this data set', function () {
     const transactions = [
@@ -121,41 +123,40 @@ describe('xirr', () => {
       { amount: 1765.89, date: '2016-01-22' },
       { amount: 1765.89, date: '2017-01-20' },
       { amount: 22421.55, date: '2017-06-05' },
-    ]
-    const result = xirr(transactions)
-    expect(result).toBeCloseTo(0.212686)
-  })
+    ];
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(0.212686);
+  });
 
   it('does not error out on this data set (see issue #2)', function () {
     const transactions = [
       { amount: -2839.2, date: '2018-01-21' },
       { amount: -207.7, date: '2018-01-24' },
       { amount: 2526, date: '2018-04-26' },
-    ]
+    ];
 
-    const result = xirr(transactions)
-    expect(result).toBeCloseTo(-0.514174)
-  })
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(-0.514174);
+  });
 
-  it.skip('converges for this coronavirus inspired data set', () => {
+  it('converges for this coronavirus inspired data set', () => {
     const transactions = [
       { amount: -713.07, date: '2020-03-04' },
       { amount: 555.33, date: '2020-03-17' },
-    ]
-    // With the right guess, the computation will converge
-    const result = xirr(transactions, { guess: -0.9975 })
-    expect(result).toBeCloseTo(-0.999106)
-  })
+    ];
 
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(-0.999106);
+  });
 
   it('converges for this data set (see issue #14)', () => {
     const transactions = [
       { amount: -99995, date: '2021-08-03' },
       { amount: 97642, date: '2021-08-09' },
-    ]
-    const result = xirr(transactions, { guess: -0.99 })
-    expect(result).toBeCloseTo(-0.765099)
-  })
+    ];
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(-0.765099);
+  });
 
   it('succeeds even when the N-R algorithm values go into the range (-∞,-1]', () => {
     const transactions = [
@@ -169,19 +170,27 @@ describe('xirr', () => {
       { amount: -2552, date: '2001-07-13' },
       { amount: -2530, date: '2001-07-16' },
       { amount: -9840, date: '2001-07-17' },
-      { amount: 38900, date: '2001-07-18' }
-    ]
+      { amount: 38900, date: '2001-07-18' },
+    ];
 
-    const result = xirr(transactions)
-    expect(result).toBeCloseTo(-0.835340)
-  })
+    const result = xirr(transactions);
+    expect(result).toBeCloseTo(-0.83534);
+  });
 
   it('takes guess as option', () => {
     const transactions = [
       { amount: -1000, date: '2010-01-01' },
       { amount: 1100, date: '2011-01-01' },
-    ]
-    const result = xirr(transactions,  { guess: 0.1 } )
-    expect(result).toBeCloseTo(0.10)
-  })
-})
+    ];
+    const result = xirr(transactions, { guess: 0.1 });
+    expect(result).toBeCloseTo(0.1);
+  });
+
+  it.each(testCases)(
+    'tests calculation with given payments',
+    (expectedResult, payments) => {
+      const result = xirr(payments);
+      expect(result).toBeCloseTo(expectedResult);
+    },
+  );
+});
